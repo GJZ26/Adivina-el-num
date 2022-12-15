99dd09d52d30f197374223362550ad6c35a8bbd5
GJZ26 <adolfogj26@gmail.com>
2022-12-14 20:47:48 -0600

    v beta.1.0.0

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # Adivina el número
 Adivina el número es un juego inspirado en un ejercicio encontrados en los cursos de [mozilla](https://developer.mozilla.org/es/docs/Learn/JavaScript/First_steps/A_first_splash#ejemplo_%E2%80%94_juego_adivina_el_n%C3%BAmero), donde el navegador elige un número aleatorio del 1-100 y el usuario debe adivinar cuál es el número elegido en solo 10 turnos.
 
+# Changelog
+
+## Version beta.1.0.0
+
+* Interfaz finalizada.
+* Lógica finalizada.
+* Dice al usuario si perdió en caso de que haya agotado sus intentos o ganado si ha adivinado el número.
+* Al finalizar, únicamente permite crear una partida nueva.
+
 ## Version 0.0.1
 Cumple con la lógica mínima básica
 * Identifica cuando el numero que ingresó el usuario coincide con el elegido.
--- js/main.js
+++ js/main.js
@@ -1,54 +1,173 @@
-/* Declaración de variables */
-let randomNumber = Math.floor(Math.random()*100)+1;
-let contadorIntento=0;
-let historial, attemptValue;
+/* Traemos a los elementos del DOM  */
+const attempsBox = document.getElementById('attemps')
+const userInput = document.getElementById('user-attemp')
+const sendBtn = document.getElementById('send')
+const resetBtn = document.getElementById('reset')
+const msgBox = document.getElementById('feedback')
+const historyAtt = document.getElementById('attemps-history')
 
-const botonEnviar = document.getElementById('attemptSubmit');
-const cuadroIntento = document.getElementById('attemptField');
-const intentos = document.getElementById('attempts');
-const turno = document.getElementById('attemptNum');
-const pista = document.getElementById('hint');
-const msg = document.getElementById('msgAttempt');
 
-function checkAttemp(){	
-	attemptValue = parseInt(cuadroIntento.value);
-	cuadroIntento.value='';
+class Game {
 
-	if(attemptValue===randomNumber){
-		pista.textContent='En el clavo!';
-		msg.textContent="¡Felicidades! Has ganado"
-	}else if(attemptValue<randomNumber){
-		msg.textContent="Incorrecto"
-		pista.textContent='número demasiado bajo';
-	}else{
-		msg.textContent="Incorrecto"
-		pista.textContent='numero demasiado alto';
+	_numberToGuess = null
+	actualAttemp = null
+	attempNumber = 1
+	attempLimit = 10
+	boxClass = ""
+	gameFinish = false
+
+	constructor() {
+		this.resetAll()
 	}
 
-	if(contadorIntento===0){
-		if (isNaN(attemptValue)){
-			historial=intentos.textContent='Intentos: ?';
-		}else{
-			historial=intentos.textContent='Intentos: '+attemptValue;
+	resetAll() {
+		this.gameFinish = false
+		this.boxClass = ""
+		this._numberToGuess = Math.floor(Math.random() * 100) + 1;
+
+		this.resetAttemps()
+		this.clearHistory()
+		this.hiddeMsgBox()
+		this.resetUserInput()
+		this.resetClasses()
+	}
+
+	resetClasses() {
+
+		sendBtn.style.display = "";
+
+		if (resetBtn.classList.contains("send"))
+			resetBtn.classList.remove("send")
+
+		if (!resetBtn.classList.contains("reset"))
+			resetBtn.classList.add("reset")
+	}
+
+	resetUserInput() {
+		userInput.value = null
+		userInput.focus()
+	}
+
+	resetAttemps() {
+		this.attempNumber = 1;
+		attempsBox.textContent = `Intento ${this.attempNumber}/${this.attempLimit}`
+	}
+
+	clearHistory() {
+		historyAtt.style.opacity = 0
+		const node = document.createElement("p")
+
+		node.classList.add("history");
+		node.textContent = "Historial"
+
+		historyAtt.innerHTML = ''
+		historyAtt.appendChild(node)
+	}
+
+	hiddeMsgBox() {
+		msgBox.textContent = "¡Escribe tu intento!"
+	}
+
+	validateEnter(event) {
+		if (event.keyCode !== 13) {
+			return
 		}
 
-	}else{
-		if(isNaN(attemptValue)){
-			intentos.textContent=historial += ', ?';
-		}else{
-			intentos.textContent=historial += ', '+attemptValue;
+		if (this.gameFinish) {
+			this.resetAll()
+			return
+		}
+
+		this.validateAttemp()
+	}
+
+	validateAttemp() {
+
+		console.log(this._numberToGuess)
+
+		this.actualAttemp = userInput.value
+		let clazz = null
+
+
+		if (this.actualAttemp == this._numberToGuess) {
+			msgBox.textContent = "¡Haz ganado!"
+			clazz = "right"
+			this.matchEnded()
+			this.gameFinish = true
+		}
+
+		if (this.actualAttemp > this._numberToGuess) {
+			msgBox.textContent = "Demasiado alto"
+			clazz = "high"
+		}
+
+		if (this.actualAttemp < this._numberToGuess) {
+			msgBox.textContent = "Damasiado bajo"
+			clazz = "low"
+		}
+
+		userInput.select()
+		this.addToHistory(clazz)
+		if (this.gameFinish)
+			return
+		this.writeAttempNumber()
+
+
+		if (this.actualAttemp !== this._numberToGuess && this.attempNumber > 10) {
+			msgBox.textContent = "Has perdido"
+			attempsBox.textContent = "Sin turnos disponibles"
+			this.matchEnded()
+			this.gameFinish = true
+			return
 		}
 	}
-	
 
-	contadorIntento++;
-	turno.textContent=contadorIntento+'/10';
+	addToHistory(clazz) {
+
+		if (!this.actualAttemp) {
+			msgBox.textContent = "¿Ya intentaste escribiendo algo?"
+			return
+		}
+
+		if (this.actualAttemp > 100) {
+			msgBox.textContent = "Creo que es demasiado alto..."
+			return
+		}
+
+		if (this.actualAttemp < 1) {
+			msgBox.textContent = "Creo que es demasiado bajo..."
+			return
+		}
+
+		if (this.attempNumber == 1) {
+			historyAtt.style.opacity = 1;
+		}
+
+		const node = document.createElement("span")
+
+		node.textContent = this.actualAttemp
+		node.classList.add("log")
+		node.classList.add(clazz)
+
+		historyAtt.appendChild(node)
+
+		this.attempNumber++
+	}
+
+	writeAttempNumber() {
+		attempsBox.textContent = `Intento ${this.attempNumber}/${this.attempLimit}`
+	}
+
+	matchEnded() {
+		sendBtn.style.display = "none";
+		resetBtn.classList.add("send")
+		resetBtn.classList.remove("reset")
+	}
+
 }
 
-function start(){
-	//console.log("%cInicializando...","color:green;");
-	//console.log("%cMantén esto en secreto\n"+"%cNúmero seleccionado: "+"%c"+randomNumber,"color: pink;","color:sky;","color:white;");
+let currentMatch = new Game()
 
-	botonEnviar.addEventListener('click',checkAttemp);
-}
-start();
+userInput.onkeydown = (e) => { currentMatch.validateEnter(e) }
+sendBtn.onclick = () => { currentMatch.validateAttemp() }
+resetBtn.onclick = () => { currentMatch = new Game() }
